78d8d4e7a556a2b2e1645d12a9f7fb7772d56ca9
300330386 <300330386@N6105S17.ad.douglascollege.ca>
2025-04-16 10:08:45 -0700

    First commit

---
+++ DemoProject/src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is Git demo project");
+		System.out.println("created by team leader");
+	}
+
+}
